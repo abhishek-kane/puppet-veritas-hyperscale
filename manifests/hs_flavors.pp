26d5ddc8fa4cf2b2438aa1ce0279576ba38c7f4f
Abhishek Kane <abhishek.kane@veritas.com>
2017-06-14 14:29:12 +0530

    Add flavors using nova_flavor

--- manifests/hs_flavors.pp
+++ manifests/hs_flavors.pp
@@ -21,32 +21,27 @@ class  veritas_hyperscale::hs_flavors (
 
     include ::openstack_integration::config
 
-	nova_flavor { 'VRTSGold':
-		ensure => present,
-		ram    => '8192',
-		disk   => '80',
-		vcpus  => '4',
-	}
+    nova_flavor { 'VRTSGold':
+        ensure      => present,
+        ram         => '8192',
+        disk        => '80',
+        vcpus       => '4',
+        properties  => {ref_factor => '2', qos_priority => '3', dn_ref_factor => '1', qos_maxiops => '30000', qos_miniops => '300'},
+    }
 
-	nova_flavor { 'VRTSSilver':
-		ensure => present,
-		ram    => '4096',
-		disk   => '40',
-		vcpus  => '2',
-	}
+    nova_flavor { 'VRTSSilver':
+        ensure      => present,
+        ram         => '4096',
+        disk        => '40',
+        vcpus       => '2',
+        properties  => {ref_factor => '1', qos_priority => '2', dn_ref_factor => '1', qos_maxiops => '20000', qos_miniops => '200'},
+    }
 
-	nova_flavor { 'VRTSBronze':
-		ensure => present,
-		ram    => '2048',
-		disk   => '20',
-		vcpus  => '1',
-	}
-
-	# Execute only once.
-	exec {'hs_flavors':
-		path        => '/usr/bin:/usr/sbin:/bin',
-		creates     => "/var/opt/VRTSofcore/.hs_flavor_configured",
-		environment => ["HOME=/root", "OS_AUTH_URL=$os_auth_url", "OS_IDENTITY_API_VERSION=$os_identity_api_ver", "OS_USERNAME=$os_user", "OS_PASSWORD=$os_passwd", "OS_PROJECT_DOMAIN_NAME=$os_project_domain_name", "OS_PROJECT_NAME=$os_project_name", "OS_USER_DOMAIN_NAME=$os_user_domain_name"],
-		command     => "/$path/scripts/hs_flavors.sh",
-	}
+    nova_flavor { 'VRTSBronze':
+        ensure      => present,
+        ram         => '2048',
+        disk        => '20',
+        vcpus       => '1',
+        properties  => {ref_factor => '0', qos_priority => '1', dn_ref_factor => '0', qos_maxiops => '10000', qos_miniops => '100'},
+    }
 }
