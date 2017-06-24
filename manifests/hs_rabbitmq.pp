6f7ddef057b9c2e9322d4e7737bc387d1bc94081
abhishek.kane <abhishek.kane@veritas.com>
2017-06-24 17:06:41 +0530

    Make changes to integrate rabbitmq module with TripleO.

--- manifests/hs_rabbitmq.pp
+++ manifests/hs_rabbitmq.pp
@@ -1,13 +1,9 @@
 # == Class: hs_rabbitmq
 #
-# === Example
-#
-#    class {'veritas_hyperscale::hs_rabbitmq':
-#    }
-#
 # === Authors
 #
 # Dnyaneshwar Pawar <dnyaneshwar.pawar@veritas.com>
+# Abhishek Kane <abhishek.kane@veritas.com>
 #
 # === Copyright
 #
@@ -16,62 +12,61 @@
 class  veritas_hyperscale::hs_rabbitmq (
 ) inherits veritas_hyperscale
 {
-    require veritas_hyperscale
-    require veritas_hyperscale::controller_pkg_inst
+  require veritas_hyperscale
 
-    ::openstacklib::messaging::rabbitmq {'vrts_hyperscale':
-       userid       => 'hyperscale',
-       password     => 'elacsrepyh',
-    }
+  ::openstacklib::messaging::rabbitmq {'vrts_hyperscale':
+    userid   => 'hyperscale',
+    password => 'elacsrepyh',
+  }
 
-    # FIXME The rabbitmq_exchange also uses rabiitmqadmin
-    # script just like hs_rabbitmq.sh
-    rabbitmq_exchange { 'hyperscale-controller':
-	user => 'hyperscale',
-	password => 'elacsrepyh',
-	type => 'direct',
-	durable => true,
-    }
+  # FIXME The rabbitmq_exchange also uses rabiitmqadmin
+  # script just like hs_rabbitmq.sh
+  rabbitmq_exchange { 'hyperscale-controller':
+    user     => 'hyperscale',
+    password => 'elacsrepyh',
+    type     => 'direct',
+    durable  => true,
+  }
 
-    rabbitmq_exchange { 'hyperscale-stats':
-	user => 'hyperscale',
-	password => 'elacsrepyh',
-	type => 'direct',
-	durable => true,
-    }
+  rabbitmq_exchange { 'hyperscale-stats':
+    user     => 'hyperscale',
+    password => 'elacsrepyh',
+    type     => 'direct',
+    durable  => true,
+  }
 
-    rabbitmq_exchange { 'hyperscale-datanode':
-	user => 'hyperscale',
-	password => 'elacsrepyh',
-	type => 'direct',
-	durable => true,
-    }
+  rabbitmq_exchange { 'hyperscale-datanode':
+    user     => 'hyperscale',
+    password => 'elacsrepyh',
+    type     => 'direct',
+    durable  => true,
+  }
 
-    rabbitmq_exchange { 'hyperscale-recv':
-	user => 'hyperscale',
-	password => 'elacsrepyh',
-	type => 'direct',
-	durable => true,
-    }
+  rabbitmq_exchange { 'hyperscale-recv':
+    user     => 'hyperscale',
+    password => 'elacsrepyh',
+    type     => 'direct',
+    durable  => true,
+  }
 
-    rabbitmq_exchange { 'hyperscale-storage':
-	user => 'hyperscale',
-	password => 'elacsrepyh',
-	type => 'direct',
-	durable => true,
-    }
+  rabbitmq_exchange { 'hyperscale-storage':
+    user     => 'hyperscale',
+    password => 'elacsrepyh',
+    type     => 'direct',
+    durable  => true,
+  }
 
-    rabbitmq_exchange { 'hyperscale-compute-hy':
-	user => 'hyperscale',
-	password => 'elacsrepyh',
-	type => 'direct',
-	durable => true,
-    }
+  rabbitmq_exchange { 'hyperscale-compute-hy':
+    user     => 'hyperscale',
+    password => 'elacsrepyh',
+    type     => 'direct',
+    durable  => true,
+  }
 
-    rabbitmq_exchange { 'hyperscale-datanode-hypervisor':
-	user => 'hyperscale',
-	password => 'elacsrepyh',
-	type => 'direct',
-	durable => true,
-    }
+  rabbitmq_exchange { 'hyperscale-datanode-hypervisor':
+    user     => 'hyperscale',
+    password => 'elacsrepyh',
+    type     => 'direct',
+    durable  => true,
+  }
 }
