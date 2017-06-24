07d87f5d770ff56fde5d0abef326bb411ffd84b4
Abhishek Kane <abhishek.kane@veritas.com>
2017-06-24 16:23:16 +0530

    Make changes to integrate this module with TripleO.

--- manifests/hs_keystone.pp
+++ manifests/hs_keystone.pp
@@ -1,52 +1,45 @@
 # == Class: hs_keystone
 #
-# === Variables
-#
-# [keystone_ip]
-#   Management IP of keystone node.
-#
-# === Example
-#
-#    class {'veritas_hyperscale::hs_keystone':
-#        keystone_ip => "172.101.101.1"
-#    }
-#
 # === Authors
 #
 # Dnyaneshwar Pawar <dnyaneshwar.pawar@veritas.com>
+# Abhishek Kane <abhishek.kane@veritas.com>
 #
 # === Copyright
 #
 # Copyright (c) 2017 Veritas Technologies LLC.
 #
 class  veritas_hyperscale::hs_keystone (
-    $keystone_ip = $keystone_ip,
 ) inherits veritas_hyperscale
 {
-    require veritas_hyperscale
-    require veritas_hyperscale::controller_pkg_inst
+  require veritas_hyperscale
 
+  include ::keystone::deps
+
+  $keystone_ip = hiera('keystone_admin_api_vip', false)
+
+  if ($keystone_ip) {
     keystone_user { 'hyperscale':
-        ensure      => present,
-        enabled     => true,
-        email       => 'hyperscale@localhost',
-        password	=> 'elacsrepyh',
+      ensure   => present,
+      enabled  => true,
+      email    => 'hyperscale@localhost',
+      password => 'elacsrepyh',
     }
 
     keystone_user_role { 'hyperscale@service':
-        roles   => ['admin'],
-        ensure  => present,
+      ensure => present,
+      roles  => ['admin'],
     }
 
     keystone_user { '_proxy_':
-        ensure      => present,
-        enabled     => true,
-        password    => 'elacsrepyh',
+      ensure   => present,
+      enabled  => true,
+      password => 'elacsrepyh',
     }
 
     keystone_user_role { '_proxy_@admin':
-        roles   => ['admin'],
-        ensure  => present,
+      ensure => present,
+      roles  => ['admin'],
     }
 
     keystone_role { 'infra_admin':
@@ -54,23 +47,24 @@ class  veritas_hyperscale::hs_keystone (
     }
 
     keystone_user_role { 'admin@admin':
-        roles   => ['admin', 'infra_admin'],
-        ensure  => present,
+      ensure => present,
+      roles  => ['admin', 'infra_admin'],
     }
 
     keystone_service { 'hyperscale':
-        name        => 'hyperscale',
-        ensure      => present,
-        description	=> 'HyperScale Infrastructure Service',
-        type    	=> 'infrastructure',
+      ensure      => present,
+      name        => 'hyperscale',
+      description => 'HyperScale Infrastructure Service',
+      type        => 'infrastructure',
     }
 
     keystone_endpoint { 'hyperscale':
-        ensure          => present,
-        type            => 'infrastructure',
-        region	    	=> 'RegionOne',
-        public_url      => "http://$keystone_ip:8753/v1/%(tenant_id)s",
-        admin_url       => "http://$keystone_ip:8753/v1/%(tenant_id)s",
-        internal_url    => "http://$keystone_ip:8753/v1/%(tenant_id)s",
+      ensure       => present,
+      type         => 'infrastructure',
+      region       => 'RegionOne',
+      public_url   => "http://$keystone_ip:8753/v1/%(tenant_id)s",
+      admin_url    => "http://$keystone_ip:8753/v1/%(tenant_id)s",
+      internal_url => "http://$keystone_ip:8753/v1/%(tenant_id)s",
     }
+  }
 }
